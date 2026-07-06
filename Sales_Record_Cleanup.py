d352ac7c5ca668aee634dd7dc3676be22b0a3332
KChavhan17 <chavhankeerti@gmail.com>
2026-07-06 15:56:21 +0530

    Sales Record

---
+++ Sales_Record_Cleanup.py
@@ -0,0 +1,15 @@
+#Sales Record Cleanup
+print("-"*70)
+print("                    Sales Record Cleanup                     ")
+print("-" * 70)
+print("\n")
+list=[50,100,150,200,250,300,350,400,450,500]
+
+print("List of daily sales:", list)
+print("\n")
+list.sort()
+print("Sorted list:", list)
+print("\n")
+list.reverse()
+print("Reversed list:", list)
+print("\n")
