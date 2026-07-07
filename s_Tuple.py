0726798e9fab909dd29befa776a417f9f22e18c7
KChavhan17 <chavhankeerti@gmail.com>
2026-07-07 16:49:43 +0530

    Updated List vs tuple program

--- s_Tuple.py
+++ s_Tuple.py
@@ -6,13 +6,16 @@ print("*"*80)
 
 # Task Queue (List): Requires frequent additions, removals, and ordering changes.
 task_queue = ["Task A", "Task B"]
-task_queue.append(["Task C","Task D"])  # Lists are mutable, ideal for dynamic queues.
+task_queue.append(["Task C","Task D"])
+task_queue.pop(0)  # Remove the first task
+  # Lists are mutable, ideal for dynamic queues.
 print("\nDynamic Queue:", task_queue)
 
 
 # GPS Coordinates (Tuple): Data must remain constant to prevent location errors.
 gps_log = (12.9716, 77.5946)  # Tuples are immutable, ensuring data integrity.
 print("\nFixed GPS Coordinates:", gps_log)
+
 try:
     gps_log[0] = 13.0827  # Attempting to modify a tuple will raise an error.
 except TypeError as e:
