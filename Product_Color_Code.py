56982835781f2faf68b9cef589bb80e2cc054168
KChavhan17 <chavhankeerti@gmail.com>
2026-07-07 16:24:51 +0530

    Updated color code

--- Product_Color_Code.py
+++ Product_Color_Code.py
@@ -1,13 +1,13 @@
 #Product Color Code Lookup
 
-print("                                Product Color Code Lookup                       ")
+print("                                Product Color Code Lookup                      ")
 print("*"*90)
 
 # Dictionary using RGB tuples as keys
 paint_stock = {
-    (255, 0, 0): 50,    # Red
-    (0, 255, 0): 35,    # Green
-    (0, 0, 255): 120    # Blue
+    (255, 0, 0): "Red",     
+    (0, 255, 0): "Green",    
+    (0, 0, 255): "Blue"   
 }
 
 # Lookup stock quantity
