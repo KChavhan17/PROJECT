b1aed57ce81e60f67ca3c4d21778af888e76f174
KChavhan17 <chavhankeerti@gmail.com>
2026-07-14 17:01:49 +0530

    Department and Employess

---
+++ Department_emp_group.py
@@ -0,0 +1,15 @@
+#Department-wise employeee grouping
+
+print("\n---Department-wise Employee Grouping---\n")
+
+my_dict={"Cyber Security":7,"Softare engineering":6,"Data Analytics":5,"Data Scientists":8}
+
+
+for i in my_dict:
+    dep=i
+    emp=my_dict[dep]
+    print(f"Department:{dep}    \n  Employees:{emp}\n")
+
+    
+
+
